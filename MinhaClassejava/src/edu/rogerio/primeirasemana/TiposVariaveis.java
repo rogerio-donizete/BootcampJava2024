fed60b73e012172398ab16cea558e5a8a445e1da
Bowie <marketing@letscode.com.br>
2024-04-29 15:13:05 -0300

    atualizando ate jva basico

---
+++ MinhaClassejava/src/edu/rogerio/primeirasemana/TiposVariaveis.java
@@ -0,0 +1,10 @@
+package edu.rogerio.primeirasemana;
+
+public class TiposVariaveis {
+    
+    public static void main(String[] args)throws Exception{
+        double salariominimo =2500.50;
+    System.out.println(salariominimo);
+
+    }
+}
---
+++ MinhaClassejava/src/edu/rogerio/primeirasemana/operadores.java
@@ -0,0 +1,8 @@
+package edu.rogerio.primeirasemana;
+
+public class operadores {
+    
+    public static void main(String[] args) {
+        
+    }
+}
