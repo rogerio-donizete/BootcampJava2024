f1571d0cdc4f2bf1ada8e7da3a66f3dc1a525bdb
Bowie <marketing@letscode.com.br>
2024-04-30 14:27:58 -0300

    ArgumentosTerminal

---
+++ MinhaClassejava/src/edu/rogerio/primeirasemana/ArgumentosTerminal.java
@@ -0,0 +1,8 @@
+package edu.rogerio.primeirasemana;
+
+public class ArgumentosTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal!");
+    }
+
+}
