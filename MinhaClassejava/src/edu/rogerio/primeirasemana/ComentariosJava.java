9af65a5391d362e216445970aba984f2dd6a3870
Bowie <marketing@letscode.com.br>
2024-04-30 11:53:31 -0300

    incluido a classe comentariosjava

---
+++ MinhaClassejava/src/edu/rogerio/primeirasemana/ComentariosJava.java
@@ -0,0 +1,14 @@
+package edu.rogerio.primeirasemana;
+
+public class ComentariosJava {
+
+    // duas barras comenta a linha
+    /*barra asterisco comenta 
+     * varias linhas
+     */
+    /** 
+     * @see Barra dois asterisco é usado para docuemntação @javdoc
+    */
+    String nota = "comentarios não são usados na compilação do programa.";
+    
+}
